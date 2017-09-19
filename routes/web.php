ba827f5b7200f54cb5e6f0d1339f1822d9d188ae
alpaycelik <alpay.celik@bil.omu.edu.tr>
2017-09-19 12:03:49 +0300

    Blade template düzenlemesi yapıldı...

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
+});
+
+Route::get('admin', function () {
+    return view('backend.index');
 });
